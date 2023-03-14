1bc59a5434de07460c6a967de0b59d56008d65c2
renanuema <65867798+renanuema@users.noreply.github.com>
2023-03-14 13:48:10 -0300

    Add files via upload

---
+++ TestandoVariaveis.java
@@ -0,0 +1,22 @@
+
+public class TestandoVariaveis {
+
+	public static void main(String[] args) {
+		System.out.println("ola novo teste");
+	
+		int idade;
+		idade = 31;
+		
+		System.out.println(idade);
+		
+		idade = 20 + 11;
+		
+		System.out.println(idade);
+		
+		idade = 5 * 6 + 1;
+		
+		System.out.println("a idade é " + idade);
+		
+	}
+	
+}
