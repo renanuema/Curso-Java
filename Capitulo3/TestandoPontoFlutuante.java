08452d48d06ec88d4d8d7ca0da8660494ec05a94
renanuema <65867798+renanuema@users.noreply.github.com>
2023-03-14 16:12:14 -0300

    Add files via upload

---
+++ Capitulo3/TestandoPontoFlutuante.java
@@ -0,0 +1,21 @@
+
+public class TestandoPontoFlutuante {
+
+	public static void main(String[] args) {
+		
+	double salario;
+	salario = 3922.90;
+		
+	System.out.println("meu salário é " + salario);
+	
+	double divisao = 3.14/2;
+	System.out.println("resultado da divisao = " + divisao);
+	
+	double divisaoInteiros = 5/2;
+	System.out.println("resultado da outra divisao = " + divisaoInteiros);	
+	
+	double divisaoDecimal = 5.0/2;
+	System.out.println("resultado da outra divisao = " + divisaoDecimal);	
+	}
+		
+}
